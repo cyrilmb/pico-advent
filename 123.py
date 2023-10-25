73233a41ecbd50295bd6e5250cb787ae8bc9a64b
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2023-10-24 23:04:04 -0500

    controlling LEDs pulsing with potentiometer

--- 123.py
+++ 123.py
@@ -89,40 +89,40 @@
 # Day 3 ~ adding buttons to activate responses on the PICO
 
 # Imports
-from machine import Pin
-import time
+# from machine import Pin
+# import time
 
-# Set button name with GPIO pin number, set the pin as INPUT and use pull down
-    #Pull down is a builtin funcionality on the controller to lower the voltage in the GPIO pin so that it 
-    #registers the 3.3V signal sent by the button. Pins can float between 0V-3.3V, so it can't detect the signal.
-button1 = Pin(13, Pin.IN, Pin.PULL_DOWN) 
-button2 = Pin(8, Pin.IN, Pin.PULL_DOWN)
-button3 = Pin(3, Pin.IN, Pin.PULL_DOWN)
+# # Set button name with GPIO pin number, set the pin as INPUT and use pull down
+#     #Pull down is a builtin funcionality on the controller to lower the voltage in the GPIO pin so that it 
+#     #registers the 3.3V signal sent by the button. Pins can float between 0V-3.3V, so it can't detect the signal.
+# button1 = Pin(13, Pin.IN, Pin.PULL_DOWN) 
+# button2 = Pin(8, Pin.IN, Pin.PULL_DOWN)
+# button3 = Pin(3, Pin.IN, Pin.PULL_DOWN)
 
-# LED names and GPIO pin numbers
-red = Pin(18, Pin.OUT)
-yellow = Pin(19, Pin.OUT)
-green = Pin(20, Pin.OUT)
+# # LED names and GPIO pin numbers
+# red = Pin(18, Pin.OUT)
+# yellow = Pin(19, Pin.OUT)
+# green = Pin(20, Pin.OUT)
 
-onboardLED = Pin(25, Pin.OUT)
+# onboardLED = Pin(25, Pin.OUT)
 
-while True: # forever and ever loop
-    time.sleep(0.2) # quick nap
+# while True: # forever and ever loop
+#     time.sleep(0.2) # quick nap
 
-    if button1.value() == 1:
-        print('NOOO YOU PUSHED THE WRONG BUTTON')
-        green.value(1)
+#     if button1.value() == 1:
+#         print('NOOO YOU PUSHED THE WRONG BUTTON')
+#         green.value(1)
 
-    elif button2.value() == 1:
-        print('WHY WOULD YOU PUSH THAT ONE')
-        yellow.value(1)
+#     elif button2.value() == 1:
+#         print('WHY WOULD YOU PUSH THAT ONE')
+#         yellow.value(1)
 
-    elif button3.value() == 1:
-        print('THANK GOD YOU DIDNT PUSH THE LAST BUTTON WAIT NOOOO')
-        red.value(1)
+#     elif button3.value() == 1:
+#         print('THANK GOD YOU DIDNT PUSH THE LAST BUTTON WAIT NOOOO')
+#         red.value(1)
     
-    else:
-        green.value(0)
-        yellow.value(0)
-        red.value(0)
-        onboardLED.toggle()
+#     else:
+#         green.value(0)
+#         yellow.value(0)
+#         red.value(0)
+#         onboardLED.toggle()
