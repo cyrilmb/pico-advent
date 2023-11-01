57869fbd17b8169e3f45e7e77cf8c010266cf2ba
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2023-11-01 15:19:59 -0500

    i can now convert the dictionary to a list sorted by key number

--- AynRandom/random.py
+++ AynRandom/random.py
@@ -1,4 +1,8 @@
-file = open("code.txt", "r")
-content = file.read()
-print(content)
-file.close()
+d = {}
+s = []
+with open("bbcode.txt") as text:
+    for line in text:
+       (key, val) = line.split()
+       d[int(key)] = val
+       s = sorted(d.items())
+    print(s)
