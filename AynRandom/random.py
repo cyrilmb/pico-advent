a9bd4c82592de031d0d8ce4ce1f7f3747b3958a8
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2023-11-01 16:10:03 -0500

    got the code to make a new list of lists from the sorted list. Now each successive list has one more tuple, creating a pyramid

--- AynRandom/random.py
+++ AynRandom/random.py
@@ -4,5 +4,18 @@ with open("bbcode.txt") as text:
     for line in text:
        (key, val) = line.split()
        d[int(key)] = val
-       s = sorted(d.items())
-    print(s)
+
+s = sorted(d.items())
+
+def divide(l, n):
+    result = []
+    start = 0
+    while start < len(l):
+        result.append(l[start:start + n])
+        start += n
+        n += 1
+    return result
+
+n = 1
+pyramid = divide(s, n)
+print(pyramid)
